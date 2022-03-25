aebcf7296ad0c03cd7f05f0027ddfcc2b826bb90
aagelii <aagelii17@gmail.com>
2022-03-24 21:14:38 -0500

    added support for all companies

--- dotData1.py
+++ dotData1.py
@@ -7,31 +7,18 @@ import seaborn as sns
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 from tqdm import tqdm
-import datetime
+
+df = pd.read_csv('train.csv', sep=',')
 
 
-
-
-#df = pd.read_csv('train.csv',index_col='Date', sep=',')
 df = pd.read_csv('train.csv', sep=',')
 
 cols = ["Close/Last", "Open", "High", "Low", "NextOpen"]
 df[cols] = df[cols].replace({'\$': '', ' ': '', ',': ''}, regex=True) # removes $ , and spaces
-# stocks = df.sort_values(by = [8, 9], kind = "mergesort") # sorts by company --> by id
-# stocks = stocks.to_numpy()
-#print(df.head())
 df = df.apply(pd.to_numeric, errors='ignore', downcast='float')
 
-stocks = df.sort_values(by = ["Company"], kind = "mergesort") # sorts by company --> by id
-#print(stocks.head())
-#stocks.to_csv('my_array.csv')
+stocks = df.sort_values(by = ["Company"], kind = "mergesort") 
 
-minmax = MinMaxScaler().fit(df.iloc[:, 2:3].astype('float32')) # Close index
-df_log = minmax.transform(df.iloc[:, 2:3].astype('float32')) # Close index
-df_log = pd.DataFrame(df_log)
-#print(df_log.head())
-
-# https://towardsdatascience.com/predicting-stock-prices-with-echo-state-networks-f910809d23d4
 n_reservoir = 500
 sparsity = 0.2
 rand_seed = 23
@@ -54,38 +41,21 @@ company_list = [] # list of companies (10)
 for company, stockCompany in stocks.groupby('Company'):
     company_list.append(company)
     stonk.append(stockCompany)
-    
-trainlen = 100
+
+
+for i in stonk:
+    i['Date'] = pd.to_datetime(i['Date'])
+    i['Date'] = i['Date'].dt.strftime('%Y-%m-%d')
+    i = i.iloc[:, 1:]
+    i = i.sort_values(by = 'Date')
+    i = i.set_index('Date')
+
+# trainlen = 1000 # change this; This is how many days in the past are we looking at
+# future = 1
+# futureTotal = 100 # change this??
+# pred_tot = np.zeros(futureTotal)
+trainlen = len(companyH_training_set) # length of what ever company we're looking at
 future = 1
-futureTotal = 100
-pred_tot = np.zeros(futureTotal)
-# maybe just focus on close?
-
-companyH = stonk[0] # company H
-companyH['Date'] = pd.to_datetime(companyH['Date'])
-companyH['Date'] = companyH['Date'].dt.strftime('%Y-%m-%d')
-companyH = companyH.iloc[:, 1:]
-companyH = companyH.sort_values(by = 'Date')
-companyH = companyH.set_index('Date')
-# print(companyH)
-
-# companyH['Open'].plot(figsize=(16,6))
-
-for i in range(0,futureTotal,future):
-    pred_training = esn.fit(np.ones(trainlen), companyH['Open'].iloc[i:trainlen+i])
-    prediction = esn.predict(np.ones(future))
-    pred_tot[i:i+future] = prediction[:,0]
-
-
-"""
-error, validation_set = run_echo(1.2, .005,2)
-
-future = 2
-plt.figure(figsize=(18,8))
-plt.plot(range(0,trainlen+future),stocks[0:trainlen+future],'k',label="target system")
-plt.plot(range(trainlen,trainlen+100),validation_set.reshape(-1,1),'r', label="free running ESN")
-lo,hi = plt.ylim()
-plt.plot([trainlen,trainlen],[lo+np.spacing(1),hi-np.spacing(1)],'k:')
-plt.legend(loc=(0.61,0.12),fontsize='x-large')
-sns.despine()
-"""
+train = companyH_training_set.to_numpy() # the 'Open' for whatever company we're predicting for 
+pred_training = esn.fit(np.ones(trainlen),train[:trainlen])
+prediction = esn.predict(np.ones(future)) # this is the prediction for the next day
